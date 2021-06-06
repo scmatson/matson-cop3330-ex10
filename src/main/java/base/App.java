7710f67fbc71e020b37a473cbbcbcc284c7d5b5d
Scott <scmatson23@gmail.com>
2021-06-06 13:17:25 -0400

    Exercise 10 Solution

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,4 +1,8 @@
 package base;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Scott Matson
@@ -34,4 +38,34 @@ Challenges
 
  */
 public class App {
+    private static final double tax = 0.055;
+
+    public static void main(String[] args) {
+        Scanner ui = new Scanner(System.in);
+
+        System.out.print("Enter the price of item 1: ");
+        double item1p = ui.nextFloat();
+        System.out.print("Enter the quantity of item 1: ");
+        double item1q = ui.nextFloat();
+
+        System.out.print("Enter the price of item 2: ");
+        double item2p = ui.nextFloat();
+        System.out.print("Enter the quantity of item 2: ");
+        double item2q = ui.nextFloat();
+
+        System.out.print("Enter the price of item 3: ");
+        double item3p = ui.nextFloat();
+        System.out.print("Enter the quantity of item 3: ");
+        double item3q = ui.nextFloat();
+
+        double sum = ((item1p * item1q) + (item2p * item2q) + (item3p * item3q));
+        double tax_amount = sum * tax;
+        double total = sum + tax_amount;
+
+        DecimalFormat df = new DecimalFormat("###.00");
+
+        System.out.println("Subtotal: $" + df.format(sum));
+        System.out.println("Tax: $" + df.format(tax_amount));
+        System.out.println("Total: $" + df.format(total));
+    }
 }
